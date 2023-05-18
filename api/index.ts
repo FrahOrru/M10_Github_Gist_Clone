192642d2e4bbd479de044c5001294a301e5e8f3b
FrahOrru <francesca.orru00@outlook.it>
2023-05-19 00:18:56 +0200

    #7

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,43 @@ const port: number = 3000;
 
 app.use(express.json());
 
-// Define your API endpoints here
+import { Request, Response } from 'express';
+
+app.get('/api/notes', (req: Request, res: Response) => {
+
+  const notes = [
+    { id: 1, title: 'Note 1', content: 'This is note 1' },
+    { id: 2, title: 'Note 2', content: 'This is note 2' },
+  ];
+  res.json(notes);
+});
+
+app.post('/api/notes', (req: Request, res: Response) => {
+
+  const { title, content } = req.body;
+
+  const newNote = { id: 3, title, content };
+
+  res.status(201).json(newNote);
+});
+
+app.put('/api/notes/:id', (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  const { title, content } = req.body;
+
+  const updatedNote = { id, title, content };
+
+  res.json(updatedNote);
+});
+
+app.delete('/api/notes/:id', (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  res.sendStatus(204);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- routes/auth.ts
+++ routes/auth.ts
@@ -3,15 +3,12 @@ import passport from 'passport';
 
 const router: express.Express = express();
 
-// GET /auth/google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-// GET /auth/google/callback
 router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req: Request, res: Response) => {
-    // Redirect or respond as needed after successful authentication
     res.redirect('/dashboard');
   }
 );
